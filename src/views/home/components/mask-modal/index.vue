7d57117060ceed47c1efcd54eebd0f20210bc4b3
timsync <ajaxsync@163.com>
2024-09-07 17:00:09 +0800

    微调弹窗

--- src/views/home/components/mask-modal/index.vue
+++ src/views/home/components/mask-modal/index.vue
@@ -53,20 +53,10 @@ const handleOk = () => {
 </script>
 
 <style scoped lang="less">
-.mask {
-  position: fixed;
-  background-color: rgba(0, 0, 0, 0.5);
-  inset: 0;
-  z-index: 9999;
-}
-
 .modal-mask {
   position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  z-index: 9999;
+  inset: 0;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   transition: opacity 0.3s ease;
@@ -78,8 +68,8 @@ const handleOk = () => {
   padding: 16px;
   background-color: #fff;
   border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
+  color: #000;
 }
 
 .modal-header {
